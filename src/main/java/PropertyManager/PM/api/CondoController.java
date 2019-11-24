c4a5541f8fe074fae03d37d2f7929237e7d5017b
Robert Beaudenon <robertbeaudenon@roberts-mbp.wireless.concordia.ca>
2019-11-24 10:33:34 -0500

    Adding Condo API

---
+++ src/main/java/PropertyManager/PM/api/CondoController.java
@@ -0,0 +1,40 @@
+package PropertyManager.PM.api;
+import PropertyManager.PM.model.Condo;
+import PropertyManager.PM.service.CondoService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.UUID;
+
+@RequestMapping("api/v1/condo")
+@RestController
+public class CondoController {
+
+    private final CondoService condoService;
+
+    @Autowired
+    public CondoController(CondoService condoService) {
+        this.condoService = condoService;
+    }
+
+    @PostMapping
+    public void addProperty(@RequestBody Condo condo) {
+        condoService.addProperty(condo);
+    }
+
+    @GetMapping(path = "{id}")
+    public Condo getPropertyById(@PathVariable("id") UUID id){
+        return CondoService.getCondo(id);
+    }
+
+    @DeleteMapping(path="{id}")
+    public void deleteProperty(@PathVariable("id") UUID id){
+        CondoService.deleteCondo(id);
+    }
+
+    //Returns null if there is no property to update.
+    @PutMapping(path="{id}")
+    public Condo updateProperty(@PathVariable("id") UUID id, @RequestBody Condo condo) {
+        return CondoService.updateCondo(id, condo);
+    }
+}
---
+++ src/main/java/PropertyManager/PM/dao/CondoDao.java
@@ -0,0 +1,87 @@
+package PropertyManager.PM.dao;
+
+import PropertyManager.PM.Database.SqlConnection;
+import PropertyManager.PM.model.Condo;
+import PropertyManager.PM.model.Property;
+import org.springframework.stereotype.Repository;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.UUID;
+
+@Repository("condo")
+public class CondoDao {
+    public static int insertCondo(UUID id, Condo condo){
+
+        String sql = "INSERT INTO condo (id, laundryIncluded, heatingIncluded, electricityIncluded, internetIncluded," +
+                "furnished, airConditioning, smokersAccepted) VALUES (\'" + id + "\', " +
+                condo.isLaundryIncluded() + ", " + condo.isHeatingIncluded() + ", " +
+                condo.isElectricityIncluded() + ", " + condo.isInternetIncluded() + ", " +
+                condo.isFurnished() + ", " + condo.isAirConditioning() + ", " + condo.isSmokersAccepted() + ");";
+        SqlConnection.executeQuery(sql, false, true);
+
+        Property property = condo.getProperty();
+        property.setPropertyID(condo.getId());
+
+        PropertyDao.insertProperty(condo.getProperty().getId(), property);
+        return 0;
+    }
+
+    public static Condo selectCondoById(UUID id) {
+
+        //selects property by id
+        Property property = PropertyDao.selectPropertyById(id);
+
+        try {
+            String sql = "SELECT * FROM condo WHERE id=\'" + property.getPropertyID() + "\';";
+            ResultSet rs = SqlConnection.executeQuery(sql);
+            if(rs.next()){
+                boolean laundryIncluded = rs.getBoolean("laundryIncluded");
+                boolean heatingIncluded = rs.getBoolean("heatingIncluded");
+                boolean electricityIncluded = rs.getBoolean("electricityIncluded");
+                boolean internetIncluded = rs.getBoolean("internetIncluded");
+                boolean furnished = rs.getBoolean("furnished");
+                boolean airConditioning = rs.getBoolean("airConditioning");
+                boolean smokersAccepted = rs.getBoolean("smokersAccepted");
+
+                return new Condo(property.getPropertyID(), property, laundryIncluded, heatingIncluded,
+                        electricityIncluded, internetIncluded, furnished, airConditioning, smokersAccepted);
+            }
+
+        } catch (SQLException e){
+            e.printStackTrace();
+        } catch (NullPointerException e) {
+            System.err.println("Condo Not FOUND! THROWING ERROR!");
+            e.printStackTrace();
+        }
+        return null;
+    }
+
+    public static int deleteCondoById(UUID id){
+       Condo condo = selectCondoById(id);
+        try{
+            String sql = "DELETE FROM condo WHERE id=\'" + condo.getId() + "\';";
+            SqlConnection.executeQuery(sql, false, true);
+            PropertyDao.deletePropertyById(condo.getProperty().getId());
+            return 0;
+        } catch (NullPointerException e){
+            e.printStackTrace();
+            return 1;
+        }
+
+    }
+
+    public static Condo updateCondoById(UUID id, Condo update){
+        Condo condo = selectCondoById(id);
+        if(condo == null || condo.getProperty() == null || condo.getProperty().getAddress() == null){
+            return null;
+        }
+        deleteCondoById(id);
+        insertCondo(update.getProperty().getPropertyID(), update);
+        condo = selectCondoById(id);
+        System.out.println(condo.getId());
+        return condo;
+    }
+
+
+}
